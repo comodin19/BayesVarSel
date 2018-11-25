7e4fe42694b7db31d6582d544e8ce843a479af8c
Gonzalo Garcia-donato <gonzalo.garciadonato@uclm.es>
2018-11-25 19:47:26 +0100

    Forgot changing something...

--- src/allBF.c
+++ src/allBF.c
@@ -128,8 +128,6 @@ double RobustBF21fun(int n, int k2, int k0, double Q)
 {
 //k2, total number of covariates in the model 
 	
-	return(1.0);
-	
 	if (k2>=n) return 1.0;	
 
 	double  T1=0.0, T2=0.0, T3=0.0;
