f92976ae24943528c1187709c6019e5162704c6e
Gonzalo Garcia-Donato <gonzalo.garciadonato@gmail.com>
2019-04-01 13:53:45 +0200

    For GibbsBvsF: Fixed variables included and started working on the documentation

--- R/GibbsBvsF.R
+++ R/GibbsBvsF.R
@@ -1,4 +1,127 @@
+#' Bayesian Variable and Factor Selection for linear regression models using Gibbs
+#' sampling.
+#'
+#' Approximate computation of summaries of the posterior distribution using a
+#' Gibbs sampling algorithm to explore the model space and frequency of
+#' "visits" to construct the estimates.
+#'
+#' This is a heuristic approximation to the function
+#' \code{\link[BayesVarSel]{Bvs}} so the details there apply also here.
+#'
+#' For cases where p>>n consider using the correction \code{\link[BayesVarSel]{pltltn}}
+#' to account for the limitation of MCMC techniques in these extremely large model spaces.
+#'
+#' The algorithm implemented is a Gibbs sampling-based searching algorithm
+#' originally proposed by George and McCulloch (1997). Garcia-Donato and
+#' Martinez-Beneito (2013) have shown that this simple sampling strategy in
+#' combination with estimates based on frequency of visits (the one here
+#' implemented) provides very reliable results.
 #' @export
+#' @param formula Formula defining the most complex regression model in the
+#' analysis. See details.
+#' @param data data frame containing the data.
+#' @param null.model A formula defining which is the simplest (null) model.
+#' It should be nested in the full model. It is compulsory that the null model
+#' contains the intercept and by default, the null model is defined
+#' to be the one with just the intercept 
+#' XXXXXXXXXXXXXXXX
+#' XXX: what is below to be revised:.
+#' @param prior.betas Prior distribution for regression parameters within each
+#' model. Possible choices include "Robust", "Liangetal", "gZellner",
+#' "ZellnerSiow" and "FLS" (see details).
+#' @param prior.models Prior distribution over the model space. Possible
+#' choices are "Constant", "ScottBerger" and "User" (see details).
+#' @param n.iter The total number of iterations performed after the burn in
+#' process.
+#' @param init.model The model at which the simulation process starts. Options
+#' include "Null" (the model only with the covariates specified in
+#' \code{fixed.cov}), "Full" (the model defined by \code{formula}), "Random" (a
+#' randomly selected model) and a vector with p (the number of covariates to
+#' select from) zeros and ones defining a model.
+#' @param n.burnin Length of burn in, i.e. number of iterations to discard at
+#' the beginning.
+#' @param n.thin Thinning rate. Must be a positive integer.  Set 'n.thin' > 1
+#' to save memory and computation time if 'n.iter' is large. Default is 1. This
+#' parameter jointly with \code{n.iter} sets the number of simulations kept and
+#' used to construct the estimates so is important to keep in mind that a large
+#' value for 'n.thin' can reduce the precision of the results
+#' @param time.test If TRUE and the number of variables is large (>=21) a
+#' preliminary test to estimate computational time is performed.
+#' @param priorprobs A p+1 dimensional vector defining the prior probabilities
+#' Pr(M_i) (should be used in the case where \code{prior.models}="User"; see
+#' the details in \code{\link[BayesVarSel]{Bvs}}.)
+#' @param seed A seed to initialize the random number generator
+#' @return \code{GibbsBvs} returns an object of class \code{Bvs} with the
+#' following elements: \item{time }{The internal time consumed in solving the
+#' problem} \item{lmfull }{The \code{lm} class object that results when the
+#' model defined by \code{formula} is fitted by \code{lm}} \item{lmnull }{The
+#' \code{lm} class object that results when the model defined by
+#' \code{fixed.cov} is fitted by \code{lm}} \item{variables }{The name of all
+#' the potential explanatory variables} \item{n }{Number of observations}
+#' \item{p }{Number of explanatory variables to select from} \item{k }{Number
+#' of fixed variables} \item{HPMbin }{The binary expression of the most
+#' probable model found.} \item{inclprob }{A \code{data.frame} with the
+#' estimates of the inclusion probabilities of all the variables.}
+#' \item{jointinclprob }{A \code{data.frame} with the estimates of the joint
+#' inclusion probabilities of all the variables.} \item{postprobdim }{Estimates
+#' of posterior probabilities of the dimension of the true model.}
+#' \item{modelslogBF}{A matrix with both the binary representation of the
+#' visited models after the burning period and the Bayes factor (log scale) of
+#' that model to the null model.}\item{priorprobs}{A p+1 dimensional vector containing values proportionals
+#' to the prior probability of a model of each dimension (from 0 to p)} \item{call }{The \code{call} to the
+#' function.}
+#' \item{C}{An estimation of the normalizing constant (C=sum BiPr(Mi), for Mi in the model space)}
+#' \item{method }{\code{gibbs}}
+#' @author Gonzalo Garcia-Donato and Anabel Forte
+#' @seealso \code{\link[BayesVarSel]{plot.Bvs}} for several plots of the result,
+#' \code{\link[BayesVarSel]{BMAcoeff}} for obtaining model averaged simulations
+#' of regression coefficients and \code{\link[BayesVarSel]{predict.Bvs}} for
+#' predictions. \code{\link[BayesVarSel]{pltltn}} for corrections on estimations for the
+#' situation where p>>n.
+#'
+#' \code{\link[BayesVarSel]{Bvs}} for exact
+#' version obtained enumerating all entertained models (recommended when
+#' p<20).
+#' @references Garcia-Donato, G. and Martinez-Beneito, M.A.
+#' (2013)<DOI:10.1080/01621459.2012.742443> On sampling strategies in Bayesian
+#' variable selection problems with large model spaces. Journal of the American
+#' Statistical Association, 108: 340-352.
+#'
+#' George E. and McCulloch R. (1997) Approaches for Bayesian variable
+#' selection. Statistica Sinica, 7, 339:372.
+#' @keywords package
+#' @examples
+#'
+#' \dontrun{
+#' data(diabetes, package="faraway")
+#' 
+#' #remove NA's and the column with the id of samples:
+#' diabetes2<- na.omit(diabetes)[,-1]
+#'
+#' #For reproducibility:
+#' set.seed(16091956)
+#' #Now run the main instruction
+#' diabetesVS<- GibbsBvsF(formula= glyhb ~ ., data=diabetes2, n.iter=100000, n.burnin=5000)
+#'
+#' summary(diabetesVS)
+#' #A plot of the dimension of the true model, meaning:
+#' #=number of fixed vars+number of factors+number of numerical variables
+#' plot(diabetesVS, option="dimension")
+#' #A joint inclusion plot
+#' plot(diabetesVS, option="joint")
+#'
+#' #Now with fixed variables:
+#' diabetesVS2<- GibbsBvsF(formula= glyhb ~ ., null.model= glyhb ~ chol+stab.glu, 
+#' 		                   data=diabetes2, n.iter=100000, n.burnin=5000)
+#' 											 
+#' 											 
+#' #and with fixed factors:
+#' diabetesVS3<- GibbsBvsF(formula= glyhb ~ ., null.model= glyhb ~ chol+stab.glu+location, 
+#' 		                   data=diabetes2, n.iter=100000, n.burnin=5000)
+#' 
+#' 
+#' }
+#'
 GibbsBvsF <-
   function(formula,
            data,
@@ -13,8 +136,6 @@ GibbsBvsF <-
            priorprobs = NULL,
            seed = runif(1, 0, 16091956)) {
 
-		cat("Recall: at this point of development null model should contain intercept\n")
-		
     formula <- as.formula(formula)
 		
     null.model<- as.formula(null.model)
@@ -37,7 +158,6 @@ GibbsBvsF <-
     fixed.cov <- dimnames(lmnull$x)[[2]]
 
     #Set the design matrix if fixed covariates present:
-    if (!is.null(fixed.cov)) {
       #Eval the full model
       lmfull = lm(formula,
                   data = data,
@@ -52,6 +172,11 @@ GibbsBvsF <-
       #check if null model is contained in the full one:
       namesnull <- dimnames(lmnull$x)[[2]]
       "%notin%" <- function(x, table) match(x, table, nomatch = 0) == 0
+						
+			#check that the null model contains the intercept:
+			if (sum(namesnull=="Intercept")==0 & sum(namesnull=="(Intercept)")==0)
+				stop("The null model should contain the intercept\n")
+						
       for (i in 1:length(namesnull)){
         if (namesnull[i] %notin% namesx) {
           cat("Error in var: ", namesnull[i], "\n")
@@ -99,35 +224,15 @@ GibbsBvsF <-
 
       p <- dim(X)[2]#Number of covariates to select from
 
-    }
-
-    #If no fixed covariates considered
-    if (is.null(fixed.cov)) {
-      #Check that all the fixed covariables are included in the full model
-      lmfull = lm(formula, data, y = TRUE, x = TRUE)
-      X.full <- lmfull$x
-      namesx <- dimnames(X.full)[[2]]
-      #remove the brackets in "(Intercept)" if present.
-      if (namesx[1] == "(Intercept)") {
-        namesx[1] <-
-          "Intercept" #namesx contains the name of variables including the intercept
-      }
-
-
-      X <- lmfull$x
-      knull <- 0
-      Y <- lmfull$y
-      p <- dim(X)[2]
-      n <- dim(X)[1]
-      #check if the number of models to save is correct
-    }
 
 		#Factors:
 		#positions is a matrix with number of rows equal to the number of regressors
 		#(either factor or numeric) and number of columns the number of columns of X
 		#Each row describes the position (0-1) in X of a regressor (several positions in case
 		#this regressor is a factor)
-		depvars<- attr(lmfull$terms, "term.labels")
+		#Works with only intercept, but not with other null models: depvars<- attr(lmfull$terms, "term.labels")
+		depvars<- setdiff(attr(lmfull$terms, "term.labels"), attr(lmnull$terms, "term.labels"))
+		
 		positions<- matrix(0, ncol=p, nrow=length(depvars))
 		for (i in 1:length(depvars)){positions[i,]<- grepl(depvars[i], colnames(X), fixed=T)}
 		#positionsX is a vector of the same length as columns has X
