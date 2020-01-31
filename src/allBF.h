7c1d66ba92a20d6e1e089d24bbcc8ab19e7e6bc6
Gonzalo Garcia-Donato <gonzalo.garciadonato@gmail.com>
2020-01-31 18:54:29 +0100

    forgotten change in .h file

--- src/allBF.h
+++ src/allBF.h
@@ -4,4 +4,5 @@ double flsBF21fun(int p, int n, int k2, int k0, double Q);
 double RobustBF21fun(int n, int k2, int k0, double Q);
 double LiangBF21fun(int n, int k2, int k0, double Q);
 double ZSBF21fun(int n, int k2, int k0, double Q);
+double Robust2BF21fun(int n, int k2, int k0, double Q);
 
